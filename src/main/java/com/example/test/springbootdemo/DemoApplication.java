886e1a35799230f7d8941bcd548e938dd1905a3a
Vorrarit Luengwattanakij <vorrarit@gmail.com>
2018-05-10 10:50:36 +0700

    internationalization 1

--- src/main/java/com/example/test/springbootdemo/DemoApplication.java
+++ src/main/java/com/example/test/springbootdemo/DemoApplication.java
@@ -2,11 +2,20 @@ package com.example.test.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 }
--- src/main/java/com/example/test/springbootdemo/HelloWorldController.java
+++ src/main/java/com/example/test/springbootdemo/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.example.test.springbootdemo;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 
 	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
 	public HelloWorldBean helloWorld() {
@@ -16,5 +24,10 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET, path = "/hello-world/{name}")
 	public HelloWorldBean helloWorld(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello, %s", name));
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world-i18n")
+	public String helloWorldI18N(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("greeting.message", null, locale);
 	}
 }
